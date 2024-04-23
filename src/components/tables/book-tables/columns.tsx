f720d8ab769830bb610d7f3072e0e9ab29217d14
Iwan Darmawan <iwansaila@gmail.com>
2024-04-24 00:53:47 +0700

    feat:layout and feature books

---
+++ src/components/tables/book-tables/columns.tsx
@@ -0,0 +1,55 @@
+'use client';
+import { ColumnDef } from '@tanstack/react-table';
+import { CellAction } from './cell-action';
+import { Book } from '@/constants/data';
+import { Checkbox } from '@/components/ui/checkbox';
+import { findAuthorsByBookIds } from '@/lib/authorToBookMatcher';
+export const columns: ColumnDef<Book>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'id',
+    header: 'ID',
+  },
+  {
+    accessorKey: 'name',
+    header: 'NAME',
+    cell(props) {
+      return `${props.row.original.title}`;
+    },
+  },
+  {
+    accessorKey: 'item_type',
+    header: 'TYPE',
+    cell: ({ row }) => `${row.original.item_type.toUpperCase()}`,
+  },
+  {
+    accessorKey: 'authors',
+    header: 'AUTHORS',
+    cell: ({ row }) => (
+      <p>{JSON.stringify(findAuthorsByBookIds(row.original.authors_ids))}</p>
+    ),
+  },
+
+  {
+    id: 'actions',
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
---
+++ src/constants/author.ts
@@ -0,0 +1,43 @@
+import { Author } from "./data";
+export const authors: Author[] = [
+    {
+      id: 1,
+      username: "author1",
+      firstname: "John",
+      lastname: "Doe",
+      books_ids: "1,4,9,14,19",
+      user_id: 1001,
+    },
+    {
+      id: 2,
+      username: "author2",
+      firstname: "Jane",
+      lastname: "Smith",
+      books_ids: "2,5,10,15,20",
+      user_id: 1002,
+    },
+    {
+      id: 3,
+      username: "author3",
+      firstname: "Michael",
+      lastname: "Johnson",
+      books_ids: "3,6,11,16",
+      user_id: 1003,
+    },
+    {
+      id: 4,
+      username: "author4",
+      firstname: "Emily",
+      lastname: "Brown",
+      books_ids: "7,12,17",
+      user_id: 1004,
+    },
+    {
+      id: 5,
+      username: "author5",
+      firstname: "David",
+      lastname: "Wilson",
+      books_ids: "8,13,18",
+      user_id: 1005,
+    },
+  ];
